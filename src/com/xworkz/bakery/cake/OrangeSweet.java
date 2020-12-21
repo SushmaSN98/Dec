a56e5ccee99f7433926f562628d48d15f8b1fdcb
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-12-21 23:15:10 +0530

    inheritance

---
+++ src/com/xworkz/bakery/cake/OrangeSweet.java
@@ -0,0 +1,17 @@
+package com.xworkz.bakery.cake;
+
+public class OrangeSweet extends Sweet {
+	private boolean orange;
+
+	public boolean isOrange() {
+		return orange;
+	}
+
+	public void setOrange(boolean orange) {
+		this.orange = orange;
+	}
+	
+	
+	
+
+}
---
+++ src/com/xworkz/bakery/cake/Sweet.java
@@ -0,0 +1,39 @@
+package com.xworkz.bakery.cake;
+
+public class Sweet {
+	
+	private String name;
+	private String shape;
+	private String colour;
+	
+	
+	public void tester() {
+		System.out.println("invoked tester");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getShape() {
+		return shape;
+	}
+
+	public void setShape(String shape) {
+		this.shape = shape;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+
+}
